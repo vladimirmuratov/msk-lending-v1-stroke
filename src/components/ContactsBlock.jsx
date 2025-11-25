738818bf891160804bf4e34c8e5f5a5023ef6be1
vladimir <vladimirmuratov@mail.ru>
2025-11-25 17:42:09 +0300

    remove address

--- src/components/ContactsBlock.jsx
+++ src/components/ContactsBlock.jsx
@@ -1,6 +1,6 @@
 import { Box, Link, Typography } from '@mui/material';
 import { email, phoneNumber, phoneNumber2 } from '@/config';
-import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
+// import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
 import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import SocialBlock from '@/components/SocialBlock';
@@ -28,11 +28,13 @@ export const ContactsBlock = memo(() => {
             <Box
                 sx={{ display: 'grid', gridTemplateColumns: { xs: '1fr', sm: '1fr 2fr' }, gap: { xs: '10px', sm: 0 } }}>
                 <Box sx={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-                    <Box sx={{ display: 'flex', gap: '10px' }}>
+
+                    {/*<Box sx={{ display: 'flex', gap: '10px' }}>
                         <MapOutlinedIcon sx={{ color: 'var(--red)' }} />
                         <Typography sx={{ fontSize: { xs: '14px', sm: '16px', } }}>г. Москва, Мичуринский пр-т
                             д.6,<br />ФГБУ ОБП</Typography>
-                    </Box>
+                    </Box>*/}
+
                     <Box sx={{ display: 'flex', gap: '10px' }}>
                         <LocalPhoneOutlinedIcon sx={{ color: 'var(--red)' }} />
                         <Box sx={{ display: 'flex', flexDirection: 'column', gap: '5px' }}>
@@ -57,7 +59,7 @@ export const ContactsBlock = memo(() => {
 
 
                 </Box>
-                <Box>
+                <Box sx={{visibility: 'hidden'}}>
                     {/*<iframe
                         className='map'
                         src="https://yandex.ru/map-widget/v1/?um=constructor%3A604ee8345ca097d664aa1c7764529b62cb0469a88360ead0ce5d5df219788165&amp;source=constructor"

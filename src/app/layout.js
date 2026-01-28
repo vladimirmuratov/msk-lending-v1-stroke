65f70ccd4908ab85ab8fc2a4c640dd106029561d
vladimir <vladimirmuratov@mail.ru>
2026-01-28 15:06:38 +0300

    refresh webmaster verification

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,23 +12,24 @@ import './globals.css';
 const montserrat = Montserrat({ subsets: ['latin', 'cyrillic'] });
 
 export const metadata = {
-    metadataBase: new URL('https://stroke.mskdoctor.ru'),
+    metadataBase: new URL('https://msk-stroke.vercel.app'),
     title: 'Лечение и реабилитация после инсульта в Москве — помощь 24/7',
     description: 'Лечение и реабилитация после инсульта в ведущих клиниках Москвы. Восстановление речи, движения, памяти. Индивидуальные программы, сопровождение 24/7.',
     keywords: ['реабилитация после инсульта, восстановление после инсульта, лечение инсульта москва, госпитализация после инсульта, восстановление речи после инсульта, реабилитация в москве, патронаж после инсульта, нейрореабилитация, физиотерапия инсульт, помощь после инсульта.'],
     other: {
-        ['yandex-verification']: '63b15477d46dd1f6',
+        // ['yandex-verification']: '63b15477d46dd1f6', // https://stroke.mskdoctor.ru
+         ['yandex-verification']: '55bf3e721d47fe09', // https://msk-stroke.vercel.app
     },
     openGraph: {
         title: 'Реабилитация после инсульта в Москве — восстановление, госпитализация, лечение. Звоните! 8 (499) 719-81-00, 24/7',
         description: 'Медицинская Сервисная Компания предлагает реабилитацию после инсульта в Москве: госпитализация без ожидания, физиотерапия, восстановление речи и памяти, патронаж пожилых пациентов. Круглосуточная поддержка 24/7.',
         images: ['/images/banner6.webp'],
-        url: 'https://stroke.mskdoctor.ru',
+        url: 'https://msk-stroke.vercel.app',
         type: 'website',
         locale: 'ru_RU',
     },
     alternates: {
-        canonical: 'https://stroke.mskdoctor.ru',
+        canonical: 'https://msk-stroke.vercel.app',
     }
 };
 
